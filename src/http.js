cd200d7b77cf4646e22a79652494a021a6eb0406
mkg1 <gehring.mg@gmail.com>
2024-02-19 20:25:59 -0500

    Function to update user selected places

--- src/http.js
+++ src/http.js
@@ -8,3 +8,21 @@ export async function fetchAvailablePlaces() {
 
     return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+    const response = await fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        body: JSON.stringify({places: places}), //need to convert places array to JSON before sending
+        headers: {
+            'Content-Type': 'application/json' //required to make sure data is successfully extracted on the backend
+        }
+    })
+
+    const resData = await response.json();
+
+    if (!response.ok) {
+        throw new Error('Failed to update places');
+    }
+
+    return resData.message
+}
