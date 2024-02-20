2cf48c69d0475e9760cb9aed98722ded251fb76c
mkg1 <gehring.mg@gmail.com>
2024-02-19 19:38:44 -0500

    Replace chained .thens with async/await

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,17 +4,27 @@ import Places from './Places.jsx';
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  useEffect(() => {
-      // fetch provided by default by browser
-      // fetch returns a promise (a js value that will eventually resolve to another value)
-      // can chain fetch with .then to define a function that will run once the promise is resolved and response exists. 
-      // another way to write fetch(...).then(...) is await const response = fetch(...) however, functional components can't be async so won't work
-      fetch('http://localhost:3000/places').then((response) => {
-        // .json() method returns another promise so can add another .then to the chain if you return response.json()
-        return response.json()
-      }).then((resData) => {setAvailablePlaces(resData.places)});
-  }, [])
+  // useEffect(() => {
+  //   function fetchPlaces
+  //     // fetch provided by default by browser
+  //     // fetch returns a promise (a js value that will eventually resolve to another value)
+  //     // can chain fetch with .then to define a function that will run once the promise is resolved and response exists. 
+  //     // another way to write fetch(...).then(...) is await const response = fetch(...) however, functional components can't be async so won't work
+  //     fetch('http://localhost:3000/places').then((response) => {
+  //       // .json() method returns another promise so can add another .then to the chain if you return response.json()
+  //       return response.json()
+  //     }).then((resData) => {setAvailablePlaces(resData.places)});
+  // }, [])
 
+  // alternative to chainging .thens: you can use async/await inside useEffect hook by wrapping the fetch in a new fetchPlaces function
+  useEffect(() => {
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
+    fetchPlaces();
+  }, [])
 
   return (
     <Places
